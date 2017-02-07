bb8b0611506b02f201c473c4f2c92878ec0d58f0
jiekekeji <jieke123>
2017-02-07 15:27:28 +0800

    做了點主頁

--- src/components/MainHot.vue
+++ src/components/MainHot.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
-        <div>热门</div>
-    </div>
+  <div>
+    <div>热门</div>
+  </div>
 </template>
 <style scoped>
 </style>
 <script>
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-        components:{
-        }
-    }
+  import md5 from 'js-md5'
+  export default{
+    data(){
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {},
+    created () {
+      console.log('md5=' + md5('123'));
+    },
+    mounted: function () {
+      console.log(this);
+
+    },
+    activated: function () {
+      console.log('activated');
+    },
+    beforeRouteEnter  (to, from, next) {
+      console.log('beforeRouteEnter');
+      next();
+    },
+    beforeRouteLeave (to, from, next) {
+      console.log('beforeRouteLeave=');
+      next();
+    },
+  }
 </script>
