3a5019261fca78423dbf1e5c420c259ed9034f53
jiekekeji <jieke123>
2017-02-09 17:27:34 +0800

    做了點主頁

--- src/components/MainHot.vue
+++ src/components/MainHot.vue
@@ -1,24 +1,48 @@
 <template>
   <div>
-    <div>热门</div>
+    <!--start 跑马灯公告-->
+    <div class="txtMarquee-top">
+      <ul class="infoList">
+        <li v-for="item in infoList">{{item}}</li>
+      </ul>
+    </div>
+    <!--end 跑马灯公告-->
   </div>
 </template>
 <style scoped>
+  .txtMarquee-top {
+    height: auto;
+    width: 100%;
+    background-color: azure;
+  }
+
+  .txtMarquee-top > .infoList {
+    width: 100%;
+    height: auto;
+  }
+
+  .infoList > li {
+    height: 0.9rem;
+    width: 100%;
+    text-align: center;
+    border: 0.1rem solid transparent;
+  }
 </style>
 <script>
   import md5 from 'js-md5'
+  require('../assets/superslide/jquery.SuperSlide.2.1.1')
   export default{
     data(){
       return {
-        msg: 'hello vue'
+        msg: 'hello vue',
+        infoList: []
       }
     },
     components: {},
     created () {
-      console.log('md5=' + md5('123'));
+      this.loadNoticeData();
     },
     mounted: function () {
-      console.log(this);
 
     },
     activated: function () {
@@ -32,5 +56,27 @@
       console.log('beforeRouteLeave=');
       next();
     },
+    methods: {
+      //加载公告数据
+      loadNoticeData(){
+        var me = this;
+        setTimeout(function () {
+          for (var i = 0; i < 10; i++) {
+            me.infoList.push(i);
+          }
+          //初始化跑马灯
+          me.$nextTick(function () {
+            console.log('v-for渲染已经完成');
+            jQuery(".txtMarquee-top").slide({
+              mainCell: ".infoList",
+              autoPlay: true,
+              effect: "topMarquee",
+              vis: 4,
+              interTime: 50
+            });
+          })
+        }, 3 * 1000);
+      }
+    }
   }
 </script>
